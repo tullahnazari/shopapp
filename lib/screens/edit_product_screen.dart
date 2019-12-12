96920decb28958c899069a9869d993453277ff91
tnaz <tullah.nazari@gmail.com>
2019-12-11 22:45:34 -0600

    changing form input focus by moving to next input smoothly

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,6 +9,9 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
 
+  //focuses on next input on keyboard after you click next
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +21,30 @@ class _EditProductScreenState extends State<EditProductScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-          child: ListView(
+          //scrollable form, if in landscape mode form widget doesnt lose data
+          child: SingleChildScrollView(
+            child: Column(
             children: <Widget>[
               TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Title'
                 ),
                 textInputAction: TextInputAction.next,
+                //when moving to next input via mobile keyboard
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Price'
+                ),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
               ),
             ],),
+          ),
         ),
       ),
       
