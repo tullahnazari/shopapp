770bff2aaee99f87805f68e1df7fa85ab7ea28af
tnaz <tullah.nazari@gmail.com>
2019-12-12 20:47:55 -0600

    setting up validation

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -58,8 +58,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState.save();
-    print(_editedProduct.ti);
+    print(_editedProduct.title);
 
   }
 
@@ -87,12 +91,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
             children: <Widget>[
               TextFormField(
                 decoration: InputDecoration(
-                  labelText: 'Title'
+                  labelText: 'Title',
                 ),
                 textInputAction: TextInputAction.next,
                 //when moving to next input via mobile keyboard
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value.';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
