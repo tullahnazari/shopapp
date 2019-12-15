114d917f97bcc4fbb9d3e5b4de1f570c08bac7b3
tnaz <tullah.nazari@gmail.com>
2019-12-14 21:52:02 -0600

    Transforming and displaying GET data, adding loading indicator while GET

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -22,11 +22,32 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
 
   var _showOnlyFavories = false;
+  var _isInit = true;
+  var _isLoading = false;
 
   @override
   void initState() {
-    Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+    // _isLoading = true;
+    // Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+    // _isLoading = false;
     super.initState();
+    
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
   }
 
   @override
@@ -75,7 +96,10 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
             ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavories),
+      body: _isLoading ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyFavories),
       
     );
   }
