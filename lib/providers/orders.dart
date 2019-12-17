c1012c3a20d2ab9bde9c9cb3bef1948c3c8f95f2
tnaz <tullah.nazari@gmail.com>
2019-12-16 23:42:20 -0600

    GET/ retrieve orders on order screen

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -27,6 +27,37 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url = 'https://notetaker-afe0d.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                      id: item['id'],
+                      price: item['price'],
+                      quantity: item['quantity'],
+                      title: item['title'],
+                    ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url = 'https://notetaker-afe0d.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
@@ -56,5 +87,8 @@ class Orders with ChangeNotifier {
     );
     notifyListeners();
   }
+
   
+
+
 }
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -71,6 +71,9 @@ class Products with ChangeNotifier{
       final response = await http.get(url);
       print(json.decode(response.body));
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+      return;
+    }
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
