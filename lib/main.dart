79cabcac0999d603e75941b11ea4807d2d013047
tnaz <tullah.nazari@gmail.com>
2019-12-19 20:10:52 -0600

    Automatically keep users logged in after user destroys app

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shopapp/screens/auth-screen.dart';
 import 'package:shopapp/screens/cart_screen.dart';
 import 'package:shopapp/screens/edit_product_screen.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
+import 'package:shopapp/screens/splash-screen.dart';
 import 'package:shopapp/screens/user_products_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
@@ -49,7 +50,11 @@ class MyApp extends StatelessWidget {
                 accentColor: Colors.deepOrange,
                 fontFamily: 'Lato',
               ),
-              home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+              home: auth.isAuth ? ProductsOverviewScreen() : 
+              FutureBuilder(future: auth.tryAutoLogin(), 
+              builder: (ctx, authResultSnapshot) => 
+              authResultSnapshot.connectionState == ConnectionState.waiting ? SplashScreen() : AuthScreen(),
+              ),
               routes: {
                 ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
                 CartScreen.routeName: (ctx) => CartScreen(),
